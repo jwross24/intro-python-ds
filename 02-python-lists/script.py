f45ceb6e53b3bf4f03cc64923e6851b57a19d5ad
Jonathan Ross <jwross24@vt.edu>
2019-02-04 14:04:00 +0800

    Finish list manipulation

--- 02-python-lists/script.py
+++ 02-python-lists/script.py
@@ -50,3 +50,40 @@ eat_sleep_area = areas[3] + areas[-3]
 
 # Print the variable eat_sleep_area
 print(eat_sleep_area)
+
+# %%
+# Use slicing to create downstairs
+downstairs = areas[:6]
+
+# Use slicing to create upstairs
+upstairs = areas[-4:]
+
+# Print out downstairs and upstairs
+print(downstairs, upstairs)
+
+# %%
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+
+# %%
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
+
+# %%
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = areas[:]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
