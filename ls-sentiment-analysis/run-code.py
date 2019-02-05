269bd82adf147b969ad14180f10156aeb11508fb
Jonathan Ross <jwross24@vt.edu>
2019-02-05 17:49:09 +0800

    Add comments, improve output

--- ls-sentiment-analysis/run-code.py
+++ ls-sentiment-analysis/run-code.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import TextBlob
 
 # Authenticate
-consumer_key = 'hjwLE0Fqf9RkcCHCgpJXeOxwA'
-consumer_secret = '43n9FtJvUeTIhRxxlzzhcJgTmazbDpdreH8DCexxPlJsckbDlJ'
+consumer_key = 'XXX'
+consumer_secret = 'XXX'
 
-access_token = '246975644-to8ejpN6U0ENVJMWzUIP33v8UIH9aBc7R1Tk7Kzs'
-access_token_secret = 'CyY1zZPANKJrmwUHreYRrH5mKU81GgchklR3eaMUah4gA'
+access_token = 'XXX'
+access_token_secret = 'XXX'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
