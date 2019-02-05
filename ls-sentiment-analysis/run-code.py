3c3da12a6cbeda551bab0fc34ca3a079a1c578a9
Jonathan Ross <jwross24@vt.edu>
2019-02-05 17:45:55 +0800

    Add comments, make cleaner

--- ls-sentiment-analysis/run-code.py
+++ ls-sentiment-analysis/run-code.py
@@ -1,20 +1,25 @@
 import tweepy
 from textblob import TextBlob
 
+# Authenticate
 consumer_key = 'hjwLE0Fqf9RkcCHCgpJXeOxwA'
-consumer_secret = 'XXX'
+consumer_secret = '43n9FtJvUeTIhRxxlzzhcJgTmazbDpdreH8DCexxPlJsckbDlJ'
 
 access_token = '246975644-to8ejpN6U0ENVJMWzUIP33v8UIH9aBc7R1Tk7Kzs'
-access_token_secret = 'XXX'
+access_token_secret = 'CyY1zZPANKJrmwUHreYRrH5mKU81GgchklR3eaMUah4gA'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+# Retrieve Tweets
 public_tweets = api.search('Lambda School')
 
 for tweet in public_tweets:
     print(tweet.text)
+
+    # Perform sentiment analysis on Tweets
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
+    print()
