23236396fc3e7efe5bbc69aed9698ac40f2caa12
Jonathan Ross <jwross24@vt.edu>
2019-02-07 20:37:20 +0800

    Finished! Add tweet cleaning, CSV functionality, names and ranking

--- ls-sentiment-analysis/twitter.py
+++ ls-sentiment-analysis/twitter.py
@@ -1,7 +1,10 @@
-import numpy as np
-import operator
-import tweepy
+import csv
 import datetime as DT
+import operator
+import re
+import tweepy
+
+from numpy import mean
 from textblob import TextBlob
 
 
@@ -12,53 +15,118 @@ consumer_secret = 'CONSUMER_SECRET_HERE'
 access_token = 'ACCESS_TOKEN_HERE'
 access_token_secret = 'ACCESS_TOKEN_SECRET_HERE'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-# Step 2 - Prepare query features
-
-# List of bootcamps with 
-bootcamps = ['Sarkozy', 'Kosciusko', 'Cope', 'Juppe', 'Fillon', 'Le Maire',
-             'Poisson']
-# Set default until_date to today, since_date to a week before
-until_date = DT.date().today()
-since_date = until_date - DT.timedelta(days=7)
+try:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    tweet_count_max = 45  # to prevent rate limiting
+except Exception:
+    print('Error: authentication failed')
+    raise
 
 
-# Step 2b - Label the sentiments
-def get_label(analysis, threshold=0):
-    if analysis.sentiment[0] > threshold:
+# Clean the Tweet according to the regex rule
+def clean_tweet(tweet):
+    pattern = '(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)'
+    return ' '.join(re.sub(pattern, ' ', tweet).split())
+
+
+# Label the sentiments
+def get_label(polarity, threshold=0):
+    if polarity > threshold:
         return 'Positive'
-    elif analysis.sentiment[0] == threshold:
+    elif polarity == threshold:
         return 'Neutral'
     else:
         return 'Negative'
 
 
-# Step 3 - retrieve Tweets and save them
-all_polarities = {}
-for bootcamp in bootcamps:
-    this_bootcamp_polarity = []
-    # Get tweets about the bootcamp between the dates
-    this_bootcamp_tweets = api.search(q=bootcamp, count=100, since=since_date,
-                                      until=until_date)
-    # Save the Tweets in a CSV
-    with open('%s_tweets.csv' % bootcamp, 'wb') as this_bootcamp_file:
-        this_bootcamp_file.write('tweet,sentiment_label\n')
-        for tweet in this_bootcamp_tweets:
-            analysis = TextBlob(tweet.text)
-            # Get the label corresponding to the sentiment
-            this_bootcamp_polarity.append(analysis.sentiment[0])
-            this_bootcamp_file.write('%s,%s\n' % (tweet.text.encode('utf8'),
-                                                  get_label(analysis)))
-    # Save the mean for final results
-    all_polarities[bootcamp] = np.mean(this_bootcamp_polarity)
+# Set default until_date to today, since_date to 3 days before
+until_date = DT.date.today()
+until_fmt = until_date.strftime('%Y-%m-%d')
 
-# Step bonus - Print a Result
-sorted_analysis = sorted(all_polarities.items(), key=operator.itemgetter(1),
-                         reverse=True)
-print('Mean Sentiment Polarity in descending order :')
-for candidate, polarity in sorted_analysis:
-    print('%s : %0.3f' % (candidate, polarity))
+since_date = until_date - DT.timedelta(days=3)
+since_fmt = since_date.strftime('%Y-%m-%d')
+
+
+def get_tweets(query, retweets_only=False, with_sentiment=True,
+               since_date=since_fmt, until_date=until_fmt):
+    tweets = []
+    polarities = []
+
+    try:
+        # Read in the Tweets from Twitter API
+        recd_tweets = tweepy.Cursor(api.search, q=[query, 'bootcamp'],
+                                    tweet_mode='extended', since=since_fmt,
+                                    until=until_fmt).items(tweet_count_max)
+        if not recd_tweets:
+            pass
+
+        # Parse Tweets for content and user
+        for tweet in recd_tweets:
+            parsed_tweet = {}
+
+            parsed_tweet['text'] = tweet.full_text
+            parsed_tweet['user'] = tweet.user.screen_name
+
+            # Add sentiments to the Tweet data
+            if with_sentiment:
+                analysis = TextBlob(clean_tweet(tweet.full_text))
+                polarity = analysis.sentiment.polarity
+                polarities.append(polarity)
+                parsed_tweet['sentiment'] = get_label(polarity)
+            else:
+                parsed_tweet['sentiment'] = 'N/A'
+
+            # Retweet handler
+            if tweet.retweet_count > 0 and retweets_only:
+                if parsed_tweet not in tweets:
+                    tweets.append(parsed_tweet)
+            elif not retweets_only:
+                if parsed_tweet not in tweets:
+                    tweets.append(parsed_tweet)
+
+        return tweets, polarities
+
+    except tweepy.TweepError as e:
+        print('Error:', str(e))
+
+
+# Output the Tweet, author, and sentiment label to CSV
+def write_tweets_to_csv(bootcamp, tweets):
+    with open('%s_tweets.csv' % bootcamp, 'w', newline='') as f:
+        field_names = ['text', 'user', 'sentiment']
+        writer = csv.DictWriter(f, fieldnames=field_names)
+
+        writer.writeheader()
+        for tweet in tweets:
+            writer.writerow(tweet)
+
+
+# List of bootcamps
+bootcamps = ['Le Wagon', 'Ironhack', 'App Academy', 'General Assembly',
+             'Bloc', 'Thinkful', 'Flatiron School', 'HackerYou',
+             'Coding Dojo', 'The Tech Academy', 'Hack Reactor', 'Actualize',
+             'Lambda School', 'Tech Talent South', 'Epicodus',
+             'Startup Institute', 'Codesmith', 'Makers Academy',
+             'Brain Station', 'Fullstack Academy']
+all_polarities = {}
+
+# Loop through the bootcamps, get Tweets and polarities
+for bootcamp in bootcamps:
+    tweets, polarities = get_tweets(bootcamp)
+    write_tweets_to_csv(bootcamp, tweets)
+    # Calculate mean polarity for each bootcamp
+    if polarities:
+        all_polarities[bootcamp] = mean(polarities)
+
+# Print the average sentiments in descending order
+if all_polarities:
+    sorted_polarities = sorted(all_polarities.items(),
+                               key=operator.itemgetter(1), reverse=True)
+    print()
+    print('Mean sentiment polarity in descending order:')
+    for bootcamp, polarity in sorted_polarities:
+        print('%s: %0.3f' % (bootcamp, polarity))
+    print()
